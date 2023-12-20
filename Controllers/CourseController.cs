8bcc508e20f0fc616c279091f59e18b214687327
Feyza Coşar <mfcosar@hotmail.com>
2023-12-20 12:56:37 +0300

    BTK Akademi Kurs Kayıt Uygulaması GitHub'a eklemek için ilk commit atıldı

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,44 @@
+﻿
+using BtkAkademi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace BtkAkademi.Controllers
+
+{
+    public class CourseController: Controller
+    {
+        public IActionResult Index() {
+
+            var model = Repository.Applications;
+            
+            return View(model); }
+
+        public IActionResult Apply()
+        { // get ile çalışır
+
+            return View();
+        }
+
+        //post ile çalışmasını istiyorsak
+        [HttpPost]
+        [ValidateAntiForgeryToken] //güvenli data almak için
+        public IActionResult Apply([FromForm] Candidate model)
+        {
+            if (Repository.Applications.Any(c => c.Email.Equals(model.Email)))
+            {
+                ModelState.AddModelError("","There is an application for you");
+
+
+            }
+            if (ModelState.IsValid) { 
+                Repository.Add(model);
+                return View("Feedback", model);
+            }
+            else return View();
+        }
+
+    }
+}
+
